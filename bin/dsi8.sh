23d11fbb0e29c4fabcd0094d8b8e2768249b0ee8
Daniel Wehner <daniel@tag1consulting.com>
2015-05-04 00:18:20 +0200

    add d8 installer script

---
+++ bin/dsi8.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo rm -rf sites/default/files/*
+sudo cp sites/default/default.settings.php sites/default/settings.php
+sudo chmod 0777 sites/default/settings.php
+sudo touch sites/default/services.yml
+sudo chmod 0777 sites/default/services.yml
+
+drush si --db-url=mysql://root:@localhost/dev_d8 -y
+drush upwd admin --password="test"
+
+drush en simpletest -y
+drush uli
